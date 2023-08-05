3dcad0a107952de4c89c0bc3d4af10127ce730f7
Gabriel Rodrigues <88407093+RodriguesGS@users.noreply.github.com>
2023-08-05 15:40:07 -0300

    Reupando os arquivos

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("body").style.backgroundColor = "#fff"
+
+function loading() {
+    document.getElementById("body").style.backgroundColor = "#000"
+    document.getElementsByClassName("box-loading")[0].style.display = "none"
+    document.getElementsByClassName("container")[0].style.display = "block"
+}
